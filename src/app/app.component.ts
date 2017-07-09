e9aa98bee33b1aff2a51243103b01a3acb47742f
SerjiVutinss <serjivutinss@gmail.com>
2017-07-09 21:01:17 +0100

    Pt.2 - Master/Detail completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 export class Hero {
   id: number;
-  name:string;
+  name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
@@ -13,8 +26,12 @@ export class Hero {
 
 export class AppComponent {
   title = 'Tour of Heros';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
 }
